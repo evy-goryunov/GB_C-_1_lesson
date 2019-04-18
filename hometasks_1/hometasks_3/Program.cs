f070175076ed7a23e1eae620da94cbf273cdd186
evy-goryunov <evy.goryunov@gmail.com>
2019-04-18 15:54:36 +0300

    сделал 3 задание

--- hometasks_1/hometasks_3/Program.cs
+++ hometasks_1/hometasks_3/Program.cs
@@ -10,6 +10,20 @@ namespace hometasks_3
 	{
 		static void Main(string[] args)
 		{
+
+			int a = 123;
+			int b = 456;
+
+			a = a + b;
+			b = b - a;
+			b = -b;
+			a = a - b;
+
+			Console.WriteLine("A равно: " + a);
+			Console.WriteLine("B равно: " + b);
+
+			Console.ReadKey();
+
 		}
 	}
 }
