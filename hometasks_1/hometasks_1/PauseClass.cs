09df747a759d376b139319765a88220fd90c642b
evy-goryunov <evy.goryunov@gmail.com>
2019-04-18 15:18:28 +0300

    вспомогательный класс

---
+++ hometasks_1/hometasks_1/PauseClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hometasks_1
+{
+	/// <summary>
+	/// вспомогательный класс
+	/// </summary>
+	class PauseClass
+	{
+		/// <summary>
+		/// ожидание нажатия клавиши
+		/// </summary>
+		public static void Pause()
+		{
+			Console.ReadKey();
+		}
+	}
+}
