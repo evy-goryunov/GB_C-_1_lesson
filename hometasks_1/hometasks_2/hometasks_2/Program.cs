ae8d14d2191b68a597647c867b4874faaf33544a
evy-goryunov <evy.goryunov@gmail.com>
2019-04-18 15:33:13 +0300

    доделал второе задание

--- hometasks_1/hometasks_2/hometasks_2/Program.cs
+++ hometasks_1/hometasks_2/hometasks_2/Program.cs
@@ -14,12 +14,21 @@ namespace hometasks_2
 			double height;
 			double bodyMassIndex;
 
+
+			Console.WriteLine("Введите ваш вес в килограммах.");
 			weight = Convert.ToDouble(Console.ReadLine());
+
+			Console.WriteLine("Введите ваш рост в метрах.");
 			height = Convert.ToDouble(Console.ReadLine());
 
 			bodyMassIndex = weight / (height * height);
 
-			Console.WriteLine(bodyMassIndex);
+			Console.WriteLine("Ваш ИМТ: " + bodyMassIndex);
+
+			if (bodyMassIndex <= 18)
+			Console.WriteLine("У вас дефицит веса.");
+			else if (bodyMassIndex > 18 && bodyMassIndex <= 28) Console.WriteLine("Ваш вес в норме.");
+			else if (bodyMassIndex > 28) Console.WriteLine("У вас лишний вес.");
 			Console.ReadKey();
 		}
 	}
