01d5f3601c56e81b51199c9cc056049644ed9887
evy-goryunov <evy.goryunov@gmail.com>
2019-04-18 15:25:06 +0300

    начал второе задание

---
+++ hometasks_1/hometasks_2/hometasks_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hometasks_2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double weight;
+			double height;
+			double bodyMassIndex;
+
+			weight = Convert.ToDouble(Console.ReadLine());
+			height = Convert.ToDouble(Console.ReadLine());
+
+			bodyMassIndex = weight / (height * height);
+
+			Console.WriteLine(bodyMassIndex);
+			Console.ReadKey();
+		}
+	}
+}
