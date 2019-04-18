d4144e82b64b394b7fa7b2b599fc2e73398d8e8a
evy-goryunov <evy.goryunov@gmail.com>
2019-04-18 15:08:38 +0300

    сделал первое задание

--- hometasks_1/hometasks_1/Program.cs
+++ hometasks_1/hometasks_1/Program.cs
@@ -10,7 +10,37 @@ namespace hometasks_1
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine();
+			String name;
+			String sureName;
+			int age;
+			int height;
+			int weight;
+
+			Console.WriteLine("Назовите ваше имя");
+			name = Console.ReadLine();
+
+			Console.WriteLine("Назовите вашу фамилию");
+			sureName = Console.ReadLine();
+
+			Console.WriteLine("Назовите свой возраст");
+			age = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("Назовите свой рост");
+			height = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("Назовите свой вес");
+			weight = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("Имя: " + name + ", Фамилия: " + sureName + ", Возраст: " + age + ", Рост: " + height + ", Вес: " + weight);
+
+			PauseClass.Pause();
+
+			Console.WriteLine($"Имя: {name}, Фамилия: {sureName}, Возраст: {age}, Рост: {height}, Вес: {weight}");
+
+			PauseClass.Pause();
+
+
+
 		}
 	}
 }
