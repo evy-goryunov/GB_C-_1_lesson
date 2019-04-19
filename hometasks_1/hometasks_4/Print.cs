bb792450f0ba5df3be70bcc410d2b9a3e1ffde7c
evy-goryunov <evy.goryunov@gmail.com>
2019-04-19 15:36:37 +0300

    new

---
+++ hometasks_1/hometasks_4/Print.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hometasks_4
+{
+
+	/// <summary>
+	/// вспомогательный класс
+	/// </summary>
+	class Print
+	{
+
+		/// <summary>
+		/// вывод в центре экрана
+		/// </summary>
+	
+
+		public static void Centered ()
+		{
+			Console.SetCursorPosition(Console.WindowWidth / 2, Console.WindowHeight / 2);
+		}
+
+	}
+}
---
+++ hometasks_1/hometasks_4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// Горюнов Евгений
+// а) Написать программу, которая выводит на экран ваше имя, фамилию и город проживания.
+// б) Сделать задание, только вывод организуйте в центре экрана
+// в) * Сделать задание б с использованием собственных методов(например, Print(string ms, int x, int y)
+
+namespace hometasks_4
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+
+			//String tx = "Евгений Горюнов, г. Пенза";
+			//Print.Centered(tx);
+
+			String name = "Евгений";
+			String sureName = "Горюнов";
+			String homeTown = "Пенза";
+
+			Print.Centered();
+			Console.WriteLine($"Имя: {name}, Фамилия: {sureName}, Город: {homeTown}");
+			Console.ReadKey();
+		}
+	}
+}
