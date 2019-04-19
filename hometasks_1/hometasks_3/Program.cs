0e9df1715dbbf8c4dba6d7366934c4cf59be287b
evy-goryunov <evy.goryunov@gmail.com>
2019-04-19 15:36:31 +0300

    new

--- hometasks_1/hometasks_3/Program.cs
+++ hometasks_1/hometasks_3/Program.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Горюнов Евгений
+// Написать программу обмена значениями двух переменных.
+// а) с использованием третьей переменной;
+// б) * без использования третьей переменной.
+
 namespace hometasks_3
 {
 	class Program
@@ -11,16 +16,36 @@ namespace hometasks_3
 		static void Main(string[] args)
 		{
 
+
+
 			int a = 123;
 			int b = 456;
+			int temp;
 
-			a = a + b;
-			b = b - a;
-			b = -b;
-			a = a - b;
+			Console.WriteLine("Введите число 0 если хотите обменять переменные с использованием третьей " +
+				"или 1 если хотите обменять переменные без использования третьей переменной");
 
-			Console.WriteLine("A равно: " + a);
-			Console.WriteLine("B равно: " + b);
+			int counut = Convert.ToInt32(Console.ReadLine());
+
+			if (counut == 1)
+			{
+				// обмен переменных
+				a = a + b;
+				b = b - a;
+				b = -b;
+				a = a - b;
+				Console.WriteLine("A равно: " + a);
+				Console.WriteLine("B равно: " + b);
+			}
+			else if (counut == 0)
+			{
+				temp = a;
+				a = b;
+				b = temp;
+				Console.WriteLine("A равно: " + a);
+				Console.WriteLine("B равно: " + b);
+			}
+			
 
 			Console.ReadKey();
 
